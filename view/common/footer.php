ae527e2d8cf6c6d6eee8d830e5a1f74fe4a4aea0
bilel-arouhal <bilelarouhal@gmail.com>
2023-05-09 17:00:16 +0200

    foot head

---
+++ view/common/footer.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="styles.css">
+<footer>
+    <div class="social-links">
+        <a href="#"><i class="fab fa-facebook"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+    </div>
+    <div class="legal-links">
+        <a href="#">Termes et conditions</a>
+        <a href="#">Politique de confidentialité</a>
+    </div>
+    <p>&copy; 2023 OSMOSE. Tous droits réservés.</p>
+</footer>
---
+++ view/common/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Titre de la page</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<header>
+		<div class="logo">
+			<img src="../../image/osmose.svg" alt="Logo de l'entreprise">
+		</div>
+		<nav>
+			<ul>
+				<li><a href="#">Accueil</a></li>
+				<li><a href="#">À propos</a></li>
+				<li><a href="#">Services</a></li>
+				<li><a href="#">Blog</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<main>
+	
+	</main>
+</body>
+</html>
