7078cc49141697667089ffbc4b764e7382dd7562
Elyrma <131140070+Elyrma@users.noreply.github.com>
2023-05-09 16:37:13 +0200

    Update footer.php

---
+++ view/common/footer.php
@@ -0,0 +1,29 @@
+<footer id="footer">
+            <div class="liste-footer">
+                <h2>Notre association</h2>
+                <ul>
+                    <li><a href="pageApropos.php">A propos</a></li>
+                    <li><a href="pageContact.php">Nous contacter</a></li>
+                    <li><a href="pageTournois.php">Nos tournois</a></li>
+                </ul>
+            </div>
+            <div class="liste-footer">
+                <h2>Droits</h2>
+                <ul>
+                    <li>Mentions légales</li>
+                    <li>CGV</li>
+                </ul>
+            </div>
+            <div class="liste-footer" id="reseaux">
+                <h2>Nos réseaux</h2>
+                <ul class="reseaux">
+                    <li><a href="https://www.instagram.com/valkyriesasso/"><img class="logos" src="public/images/logo_insta_blanc.png"></a></li>
+                    <li><a href="https://twitter.com/ValkyriesAsso"><img class="logos" src="public/images/logo_twitter_blanc.png"></a></li>
+                    <li><a href="https://www.twitch.tv/valkyriesasso"><img class="logos" src="public/images/logo_twitch_blanc.png"></a></li>
+                    <li><a href="https://discord.gg/KssFsxNrWx"><img class="logos" src="public/images/logo_discord_blanc.png"></a></li>
+                </ul>
+            </div>
+        </footer>
+    
+    </body>
+</html>
