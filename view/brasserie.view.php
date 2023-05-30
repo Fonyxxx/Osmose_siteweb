8acdbbc5e224072dcecfc610885d39427779f34a
Fonyxxx <tyty.etienne77@gmail.com>
2023-05-30 10:26:57 +0200

    oui

---
+++ view/brasserie.view.php
@@ -0,0 +1,24 @@
+<section>
+        <h2>Qui sommes nous ?</h2>
+        <div class="brand-info">
+            <p>Bienvenue sur le site web d'Osmose ! <br> Nous sommes une micro-brasserie située à Moissy Cramayel. <br>
+            Notre concept est de proposer une expérience gustative unique <br> en associant une bière à une région française et à un fromage local.</p>
+            <img src="grand_est_wbck.png" alt="Notre marque" class="brand-logo">
+        </div>
+    <h2>Comment brassons nous nos bières ?</h2>
+    <h2>Étape 1: Le Maltage</h2>
+    <div class="step">
+    <p>La première étape du processus de brassage consiste à faire germer les grains d'orge pour libérer les sucres nécessaires à la fermentation. Les grains sont trempés dans l'eau, puis étalés et laissés à germer pendant environ cinq jours.</p>
+    <img src="maltage.jpg" alt="Maltage" class="step-image">
+    </div>
+    <h2>Étape 2: Le Brassage</h2>
+    <div class="step">
+    <p>Après le maltage, les grains germés, appelés "malt", sont écrasés et ajoutés à de l'eau chaude dans un cuve de brassage.<br> Cette étape permet de libérer les sucres du malt et de créer une solution sucrée appelée "moût".</p>
+    <img src="empatage.jpg" alt="Brassage" class="step-image">
+    </div>
+    <h2>Étape 3: La Fermentation</h2>
+    <div class="step">
+    <p>Le moût est refroidi et ajouté à des levures dans une cuve de fermentation.<br> Les levures convertissent les sucres en alcool et en gaz carbonique, créant ainsi de la bière. <br>Cette étape peut prendre de quelques jours à plusieurs semaines, selon le type de bière.</p>
+    <img src="fermentation.jpg" alt="Fermentation" class="step-image">
+    </div>
+</section>
