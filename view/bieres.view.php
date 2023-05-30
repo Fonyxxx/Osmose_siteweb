691db86dc5e689b9f42895c795f746d3db323a9a
issraaarsy <issraa.mmi@gmail.com>
2023-05-30 09:57:45 +0200

    codes issraa

---
+++ view/bieres.view.php
@@ -0,0 +1,107 @@
+<section class="nos_bieres">
+    <div class="text_nos_bieres">
+        <h1>Nos bières</h1>
+    </div>
+    <div class="bieres">
+        <div class="bloc_biere">
+            <img src="images/bieres/grand_est.png">
+            <div class="text_bieres">
+                <h3>La Grand-Est</h3>
+                <h5>4,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_biere">
+            <img src="images/bieres/normandie.png">
+            <div class="text_bieres">
+                <h3>La Normande</h3>
+                <h5>4,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_biere">
+            <img src="images/bieres/occitanie.png">
+            <div class="text_bieres">
+                <h3>L'Occitane</h3>
+                <h5>4,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_biere">
+            <img src="images/bieres/corse.png">
+            <div class="text_bieres">
+                <h3>La Corse</h3>
+                <h5>4,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_biere">
+            <img src="images/bieres/loire.png">
+            <div class="text_bieres">
+                <h3>La Pays-de-la-Loire</h3>
+                <h5>4,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_biere">
+            <img src="images/bieres/hauts.png">
+            <div class="text_bieres">
+                <h3>La Hauts-de-France</h3>
+                <h5>4,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_biere">
+            <img src="images/bieres/provence.png">
+            <div class="text_bieres">
+                <h3>La Provence-Alpes-Côte d’Azur</h3>
+                <h5>4,90 €</h5>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="prochaines_bieres">
+    <div class="text_prochainement">
+        <h1>Prochainement</h1>
+    </div>
+    <div class="bieres_mystere">
+        <div class="bloc_prochainement">
+            <img src="images/prochainement/biere_mystere_1.png">
+            <div class="text_prochainement">
+                <h3>La Bourgogne Franche-Comté</h3>
+                <h5>4,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_prochainement">
+            <img src="images/prochainement/biere_mystere_2.png">
+            <div class="text_prochainement">
+                <h3>La Centre-Val-de-Loire</h3>
+                <h5>4,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_prochainement">
+            <img src="images/prochainement/biere_mystere_3.png">
+            <div class="text_prochainement">
+                <h3>La Bretagne</h3>
+                <h5>4,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_prochainement">
+            <img src="images/prochainement/biere_mystere_4.png">
+            <div class="text_prochainement">
+                <h3>La Nouvelle-Aquitaine</h3>
+                <h5>4,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_prochainement">
+            <img src="images/prochainement/biere_mystere_5.png">
+            <div class="text_prochainement">
+                <h3>L'Auvergne Rhône-Alpes</h3>
+                <h5>4,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_prochainement">
+            <img src="images/prochainement/biere_mystere_6.png">
+            <div class="text_prochainement">
+                <h3>L'Ile-de-France</h3>
+                <h5>4,90 €</h5>
+            </div>
+        </div>
+    </div>
+</section>
--- view/box.view.php
+++ view/box.view.php
@@ -1,12 +1,107 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<section class="nos_box">
+    <div class="text_nos_box">
+        <h1>Nos box</h1>
+    </div>
+    <div class="box">
+        <div class="bloc_box">
+            <img src="images/box/box_grand_est.png">
+            <div class="text_bieres">
+                <h3>Grand-Est</h3>
+                <h5>11,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_box">
+            <img src="images/box/box_normandie.png">
+            <div class="text_bieres">
+                <h3>Normande</h3>
+                <h5>11,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_box">
+            <img src="images/box/box_occitanie.png">
+            <div class="text_bieres">
+                <h3>Occitane</h3>
+                <h5>11,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_box">
+            <img src="images/box/box_corse.png">
+            <div class="text_bieres">
+                <h3>Corse</h3>
+                <h5>11,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_box">
+            <img src="images/box/box_pays_loire.png">
+            <div class="text_bieres">
+                <h3>Pays-de-la-Loire</h3>
+                <h5>11,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_box">
+            <img src="images/box/box_hauts_france.png">
+            <div class="text_bieres">
+                <h3>Hauts-de-France</h3>
+                <h5>11,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_box">
+            <img src="images/box/box_provence.png">
+            <div class="text_bieres">
+                <h3>Provence-Alpes-Côte d’Azur</h3>
+                <h5>11,90 €</h5>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="nos_box">
+    <div class="text_nos_box">
+        <h1>Prochainement</h1>
+    </div>
+    <div class="box">
+        <div class="bloc_box">
+            <img src="">
+            <div class="text_box">
+                <h3>Bourgogne Franche-Comté</h3>
+                <h5>11,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_box">
+            <img src="">
+            <div class="text_box">
+                <h3>Centre-Val-de-Loire</h3>
+                <h5>11,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_box">
+            <img src="">
+            <div class="text_box">
+                <h3>Bretagne</h3>
+                <h5>11,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_box">
+            <img src="">
+            <div class="text_box">
+                <h3>Nouvelle Aquitaine</h3>
+                <h5>11,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_box">
+            <img src="">
+            <div class="text_box">
+                <h3>Auvergne Rhône-Alpes</h3>
+                <h5>11,90 €</h5>
+            </div>
+        </div>
+        <div class="bloc_box">
+            <img src="">
+            <div class="text_box">
+                <h3>Ile-de-France</h3>
+                <h5>11,90 €</h5>
+            </div>
+        </div>
+    </div>
+</section>
