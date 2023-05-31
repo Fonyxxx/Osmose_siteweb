e5f75d8025471b2cce1f307b375614d819a6b225
Elyrma <131140070+Elyrma@users.noreply.github.com>
2023-05-31 18:40:10 +0200

    Pour Lina page event

---
+++ view/evenements.view.php
@@ -0,0 +1,58 @@
+<main>
+            <div class="container">
+
+                <h1>Nos évènements</h1>
+
+                <div class="case1">
+
+                    <div class="gauche">
+                        <h2>Le Festival Enzyme</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                            Ad veniam eligendi modi eius alias, accusantium voluptates quo dolorem debitis voluptatem dicta dolor
+                            corporis nulla possimus libero ha</p>
+                    </div>
+
+                    <div class="droite">
+                    <img id="logo_enzyme" src="" alt="logo_enzyme">
+                    </div>
+                </div>
+
+                <div class="case2">
+
+                    <div class="gauche">
+                    <img id="image_fromage" src="" alt="image_fromage">
+                    </div>
+
+                    <div class="droite">
+                        <h2>Atelier</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                            Ad veniam eligendi modi eius alias, accusantium voluptates quo dolorem debitis voluptatem dicta dolor
+                            corporis nulla possimus libero ha</p>
+                    </div>
+
+                </div>
+
+                <div class="case1">
+
+                    <div class="gauche">
+                        <h2>Atelierbis</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                            Ad veniam eligendi modi eius alias, accusantium voluptates quo dolorem debitis voluptatem dicta dolor
+                            corporis nulla possimus libero ha</p>
+                    </div>
+
+                    <div class="droite">
+                    <img id="jsp" src="" alt="jsp">
+                    </div>
+                </div>
+
+
+
+            </div>
+
+
+
+
+
+    </main>
+
