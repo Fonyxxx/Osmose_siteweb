0c4e6b29ca837a7f06017bf165c6dae5dfd77b1e
Fonyxxx <tyty.etienne77@gmail.com>
2023-06-02 21:26:23 +0200

    ENVIE DE CANER

--- view/index.view.php
+++ view/index.view.php
@@ -1,8 +1,93 @@
 <body>
-<header>
-</header>
-<main>
+    <main>
+        <div id="haut_gauche"><img src="public/images/haut_gauche.svg"></div>
 
-<h3>aled</h3>
+        <div id="cadre">
+            <div class="blocs_content" id="contenu">
+
+                <h1>Bienvenue sur le site d'Osmose</h1>
+                <h2>Avez-vous plus de 18 ans ?</h2>
+            
+                <div id="boutons_el">
+
+                    <input class="bouton" id="bouton_oui" type="button" onclick="location.href='accueil.php'" value="OUI";/>
+                    <input class="bouton" id="bouton_non" type="button" onclick="refus()" value="NON";/>
+
+                    <p id="refus_age"></p>
+                    
+                </div>
+            </div>
+            
+            <div class="blocs_content" id="bas_age">
+            <h5>L’abus d’alcool est dangereux pour la santé, à consommer avec modération</h5>
+            </div>
+        </div>
+
+
+        <div id="bas_droite"><img src="public/images/bas_droite.svg" id="test_position"></div>
+    </main>
+</body>
+
+
+ <!------Lien JS------>
+ <script type="text/javascript" src="public/js/index.js"></script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--
+
+
+<body id="example1" id="example2">
+<main>
+<style> 
+#example1 {
+  background-image: url(public/images/bas_droite.svg), url(public/images/haut_gauche.svg);
+  background-position: right bottom, left top;
+  background-repeat: no-repeat, no-repeat;
+  position: relative;
+  z-index: 3;
+}
+
+#example2{
+  position: relative;
+  z-index: 0;
+  background-color: blueviolet;
+
+}
+
+</style>
+<h1>Multiple Backgrounds</h1>
 
 </main>
+</body>
+
+
+
+
+<!--<body id="les_fonds">
+<header>
+</header>
+<main id="background">
+<h3>aled</h3>
+<div>
+    <h2>drfgthyujiko</h2>
+    <p>fyuhcvguhijhvhgh</p>
+</div>
+
+</main>
+
+
