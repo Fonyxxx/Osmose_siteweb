6b938d3f6023ed5a812a1f0ffa444fe94d13df3b
Elyrma <131140070+Elyrma@users.noreply.github.com>
2023-06-02 21:45:56 +0200

    mbon

--- view/index.view.php
+++ view/index.view.php
@@ -1,4 +1,8 @@
+<body>
+<header>
+</header>
 <main>
 
 <h3>aled</h3>
+
 </main>
---
+++ view/presentation.view.php
@@ -0,0 +1,14 @@
+<main>
+    <div class="container_presentation"> 
+        <div class="page_précédente">
+            <h3>Retournez sur la page précédente</h3>
+        </div>
+
+        <div class="presentation">
+
+        </div>
+
+    </div>
+
+
+</main>
