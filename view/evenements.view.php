8b01772022e5821155affceee20bdf156849604a
Fonyxxx <tyty.etienne77@gmail.com>
2023-06-04 17:51:58 +0200

    event

--- view/evenements.view.php
+++ view/evenements.view.php
@@ -1,58 +1,32 @@
-<main>
-            <div class="container">
+<div id="all_event">
 
-                <h1>Nos évènements</h1>
+    <div class="bloc_event">
+        <h1>Le Festival Enzyme</h1>
+        <div class="contenu_event">
+            <p>Le 7 juin 2023 au bâtiment H de l'Université Paris-Est Créteil (240 rue de la Motte, Moissy-Cramayel), sur le campus de Sénart, se déroulera le tant attendu festival Enzyme.<br>
+            Durant cet événement, vous retrouverez notre stand Osmose où vous pourrez découvrir notre bière phare La Grand-Est, ainsi que le fromage qui l'accompagne. Nous vous invitons à participer à l'expérience gustative inédite de l'accord bière/fromage.<br>
+            Nous vous attendons nombreux/ses !</p>
+            <img src="public/images/logo_enzyme.png">
+        </div>
+    </div>
 
-                <div class="case1">
+    <div class="bloc_event">
+        <h1>L'Atelier Dégustation</h1>
+        <div class="contenu_event">
+            <img src="public/images/fromages.jpg">
+            <p>A travers l'atelier Osmose Dégustation, qui a lieu tous les jeudis, nous vous proposons un voyage sensoriel à travers la France métropolitaine, à la découverte des saveurs uniques de nos bières et de leur accord parfait avec le fromage.<br>
+            Venez nombreux/ses, pour particper aux ateliers dégustation d'Osmose !</p>
+        </div>
+    </div>
 
-                    <div class="gauche">
-                        <h2>Le Festival Enzyme</h2>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                            Ad veniam eligendi modi eius alias, accusantium voluptates quo dolorem debitis voluptatem dicta dolor
-                            corporis nulla possimus libero ha</p>
-                    </div>
+    <div class="bloc_event">
+        <h1>L'Atelier Découverte</h1>
+        <div class="contenu_event">
+            <p>Lors de cet atelier, nous vous invitons à rencontrer les brasseurs d'Osmose, qui vous permettront de découvrir l'atmosphère animée de notre brasserie, où les odeurs de malt et de houblon flottent dans l'air.<br>
+            Nos brasseurs partagent leur passion et leur expertise en expliquant les différentes étapes du processus de brassage, depuis la sélection des ingrédients jusqu'au conditionnement.<br>
+            Atelier ouvert tous les 1er et 3e mercredis du mois.</p>
+            <img src="public/images/brassage2.png">
+        </div>
+    </div>
 
-                    <div class="droite">
-                    <img id="logo_enzyme" src="" alt="logo_enzyme">
-                    </div>
-                </div>
-
-                <div class="case2">
-
-                    <div class="gauche">
-                    <img id="image_fromage" src="" alt="image_fromage">
-                    </div>
-
-                    <div class="droite">
-                        <h2>Atelier</h2>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                            Ad veniam eligendi modi eius alias, accusantium voluptates quo dolorem debitis voluptatem dicta dolor
-                            corporis nulla possimus libero ha</p>
-                    </div>
-
-                </div>
-
-                <div class="case1">
-
-                    <div class="gauche">
-                        <h2>Atelierbis</h2>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                            Ad veniam eligendi modi eius alias, accusantium voluptates quo dolorem debitis voluptatem dicta dolor
-                            corporis nulla possimus libero ha</p>
-                    </div>
-
-                    <div class="droite">
-                    <img id="jsp" src="" alt="jsp">
-                    </div>
-                </div>
-
-
-
-            </div>
-
-
-
-
-
-    </main>
-
+</div>
